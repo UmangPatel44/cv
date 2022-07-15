ac806351101d56973a63d7112064ce7a5274fcc6
UmangPatel44 <98571349+UmangPatel44@users.noreply.github.com>
2022-07-15 17:07:11 +0530

    Added initial cv files

---
+++ Html/prog.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test,x,y;
+	cin>>test;
+	for(int i=0;i<test;i++)
+	{
+	    cin>>x>>y;
+	    if(x>=y)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+// 	for(int i=0;i<test;i++)
+// 	{
+	    
+// 	}
+	
+	return 0;
+}
